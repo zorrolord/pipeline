ade5eaf970cc44571179895d27eaeb16b520a9df
zorrolord <robert-stueckler@gmx.at>
2021-06-16 11:01:04 +0200

    Initiate person library

---
+++ PersonLib/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Zorrolord.Model.PersonLib
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+
+        public Person() { }
+
+        public Person(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public string FirstName
+        {
+            get => this.firstName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception($"{nameof(Person)}:{nameof(FirstName)}");
+                }
+
+                this.firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get => this.lastName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception($"{nameof(Person)}:{nameof(LastName)}");
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName}";
+        }
+    }
+}
